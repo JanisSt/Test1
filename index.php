309034273b0fd1333702e1658e84fa43ba7fae8a
JanisSt <j.stals@gmail.com>
2020-09-16 11:30:51 +0300

    Final all bug fixes

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 
 class Person{
     protected string $name;
-    protected ?string  $midlname;
     protected string $surname;
+    protected ?string  $midlname;
 
-    public function __construct(string $name, string $midlname = null, string $surname)
+
+    public function __construct(string $name , string $midlname = null, string $surname)
     {
         $this ->name = $name;
         $this -> midlname=$midlname;
@@ -13,15 +14,36 @@ class Person{
 
     }
 
-    public function surname(): ?string{
+    public function surname(): string{
         return $this->surname;
+
+    }
+    public function name(): ?string{
+        return $this->name;
+    }
+    public function midlname(): ?string{
+        return $this->midlname;
     }
 }
 
 $person = new Person( 'Janis', 'Ivars', 'Stals');
 
 $person2 = new Person('Ilze', null, 'Bieza');
-var_dump($person, $person2);
+
+$person3 = new Person('Niksons', 'Ze', 'Chan');
+
+echo $person->name() . PHP_EOL;
+echo $person->midlname() . PHP_EOL;
+echo $person->surname() . PHP_EOL;
+echo "----------------" . PHP_EOL;
+echo $person2->name() . PHP_EOL;
+echo $person2->midlname() . PHP_EOL;
+echo $person2->surname() . PHP_EOL;
+echo "----------------" . PHP_EOL;
+echo $person3->name() . PHP_EOL;
+echo $person3->midlname() . PHP_EOL;
+echo $person3->surname() . PHP_EOL;
+
 
 
 
